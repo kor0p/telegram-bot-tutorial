64f900de319f140ef18cc4a2d3adc9667cfb901e
kor0p <viktorua25.03@gmail.com>
2022-05-25 19:50:19 +0300

    [Урок #3] Підключаємо базу даних, та переписуємо попередній урок під запис вводу користувача не в словник, а у базу даних

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 WEBHOOK_URL = '<Тут публічний URL на сервер від ngrok>'
 BOT_TOKEN = '<Токен твого бота>'
+DB_URL = 'dbname=<назва бази даних> user=<postgres або інший користувач БД>'
--- main.py
+++ main.py
@@ -1,27 +1,55 @@
+from functools import wraps
+
+import psycopg2
+from psycopg2.extensions import cursor
+
 from telebot import TeleBot, types
 
-from config import BOT_TOKEN
+from config import BOT_TOKEN, DB_URL
 
 bot = TeleBot(BOT_TOKEN)
+connection = psycopg2.connect(DB_URL)
 
 
 users = {}
 
 
+def with_cursor(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        with connection:
+            with connection.cursor() as cr:
+                return fn(cr, *args, **kwargs)
+
+    return wrapper
+
+
 @bot.message_handler(commands=['start'])
-def start_command(msg: types.Message):
-    bot.send_message(msg.chat.id, 'Введи своє ім\'я:')
-    bot.register_next_step_handler(msg, get_user_name)
+@with_cursor
+def start_command(cr: cursor, msg: types.Message):
+    id = msg.from_user.id
+    name = msg.from_user.full_name
 
+    cr.execute(
+        'SELECT 1 FROM users WHERE id = %s',
+        (id,),
+    )
+    if cr.fetchone():
+        send_info(cr, msg)
+        return
 
-def get_user_name(msg: types.Message):
-    name = msg.text
-    users[msg.from_user.id] = {'name': name}
+    cr.execute(
+        'INSERT INTO users (id, name) VALUES (%s, %s)',
+        (id, name),
+    )
+
     bot.send_message(msg.chat.id, f'Отже, {name}, скільки тобі років?')
     bot.register_next_step_handler(msg, get_user_age)
 
 
-def get_user_age(msg: types.Message):
+@with_cursor
+def get_user_age(cr: cursor, msg: types.Message):
+    id = msg.from_user.id
     age = msg.text
     try:
         age = int(age)
@@ -30,14 +58,23 @@ def get_user_age(msg: types.Message):
         bot.register_next_step_handler(msg, get_user_age)
         return
 
-    user = users[msg.from_user.id]
-    user['age'] = age
+    cr.execute(
+        'SELECT name FROM users WHERE id = %s',
+        (id,),
+    )
+    name, = cr.fetchone()
 
-    bot.send_message(msg.chat.id, f'Чудово, {user["name"]}! Тепер введи свою стать (Ч або Ж):')
+    cr.execute(
+        'UPDATE users SET age = %s WHERE id = %s',
+        (age, id),
+    )
+
+    bot.send_message(msg.chat.id, f'Чудово, {name}! Тепер введи свою стать (Ч або Ж):')
     bot.register_next_step_handler(msg, get_user_gender)
 
 
-def get_user_gender(msg: types.Message):
+@with_cursor
+def get_user_gender(cr, msg: types.Message):
     gender = msg.text
 
     if gender not in ('Ч', 'Ж'):
@@ -45,10 +82,22 @@ def get_user_gender(msg: types.Message):
         bot.register_next_step_handler(msg, get_user_gender)
         return
 
-    user = users[msg.from_user.id]
-    user['gender'] = gender
+    cr.execute(
+        'UPDATE users SET gender = %s WHERE id = %s',
+        (gender, msg.from_user.id),
+    )
+
+    send_info(cr, msg)
+
+
+def send_info(cr: cursor, msg: types.Message):
+    cr.execute(
+        'SELECT name, age, gender FROM users WHERE id = %s',
+        (msg.from_user.id,),
+    )
+    name, age, gender = cr.fetchone()
 
     bot.send_message(
         msg.chat.id,
-        f'Ура!\nТвоє ім\'я: {user["name"]}\nТвій вік: {user["age"]}\nТвоя стать: {user["gender"]}',
+        f'Ура!\nТвоє ім\'я: {name}\nТвій вік: {age}\nТвоя стать: {gender}',
     )
